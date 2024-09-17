74523b030e40a348d14a33fd64bdcf4bdb72a3a4
Barbora Šťávová <barbora.stavova@outlook.cz>
2024-09-17 08:31:43 +0200

    Hello_world!

---
+++ example-app/app/Http/Controllers/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    //Adresář app obsahuje základní kód vaší aplikace. Brzy prozkoumáme tento adresář podrobněji; téměř všechny třídy ve vaší aplikaci však budou v tomto adresáři.
+
+}
